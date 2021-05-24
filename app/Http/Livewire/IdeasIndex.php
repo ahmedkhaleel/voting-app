aaa1f002478ab0b5e94a16879baf64ab9c1ef821
ahmedkhaleel <duZhuqkyptu8barnuz>
2021-05-24 02:09:12 +0200

    Category Filters

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -2,23 +2,57 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
 use App\Models\Vote;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 
 class IdeasIndex extends Component
 {
-//use WithPagination;
+use WithPagination;
+        public $status ;
+        public $category;
+
+    protected $queryString = [
+        'status',
+        'category',
+
+
+    ];
+    protected $listeners = ['queryStringUpdatedStatus'];
+    public function queryStringUpdatedStatus($newStatus)
+    {
+        $this->status = $newStatus;
+        $this->resetPage();
+    }
+//
+//    public function updatingStatus()
+//    {
+//        $this->resetPage();
+//    }
+public function mount(){
+        $this->status = request()->status ?? 'All';
+
+}
+
+    public function updatingCategories()
+    {
+        $this->resetPage();
+    }
     public function render()
     {
         $statuses = Status::all()->pluck('id','name');
-
+        $categories = Category::all();
         return view('livewire.ideas-index', [
             'ideas' =>  Idea::with('user','category','status')
-                ->when(request()->status && request()->status !== 'All',function($query) use ($statuses) {
-                    return $query->where('status_id', $statuses->get(request()->status));
+                ->when($this->status && $this->status !== 'All',function($query) use ($statuses) {
+                    return $query->where('status_id', $statuses->get($this->status));
+                })
+                ->when($this->category && $this->category !== 'All Category',function($query) use ($categories) {
+                    return $query->where('category_id', $categories->pluck('id','name')->get($this->category));
                 })
                 ->addSelect(['voted_by_user'=> Vote::select('id')
                     ->where('user_id', auth()->id())
@@ -26,7 +60,8 @@ class IdeasIndex extends Component
                 ])
                 ->withCount('votes')
                 ->orderByDesc('id','desc')
-                ->simplePaginate(Idea::PAGINATION_COUNT)
+                ->simplePaginate(Idea::PAGINATION_COUNT),
+            'categories' =>$categories,
 
         ]);
     }
